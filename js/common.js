0775ada9739869cab1f011f5ec3bc260b037bb96
n0m0d <n0m0d95@gmail.com>
2017-10-11 00:55:48 +0300

    11.10.17

--- js/common.js
+++ js/common.js
@@ -1,5 +1,19 @@
 $(function() {
 
-	// Custom JS
+	/* 
+	==============================
+	Custom Select "Selectize"
+	============================== 
+	*/
+	$('.city-filter').selectize({
+		create: true,
+		fortField: 'text'
+	});
+
+	$(".toggle-menu").on("click", function() {
+		$(this).toggleClass("on");
+		$(".toggle-mobile-menu").slideToggle();
+		return false;
+	});
 
 });
